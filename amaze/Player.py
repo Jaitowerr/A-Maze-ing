89efc92849c82ecf866314a8efcb0b77bd1b8d12
Joseph-dev04 <jpajuelo.dev@gmail.com>
2026-05-06 14:24:46 +0200

    Delete amaze directory

--- amaze/Player.py
+++
@@ -1,8 +0,0 @@
-class Player():
-    def __init__(self, direction: tuple[int,int]):
-        self._x, self._y = direction
-    
-    def set_x(self, num):
-        self._x += num
-    def set_y(self, num):
-        self._y += num
