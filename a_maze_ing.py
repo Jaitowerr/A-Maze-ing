f6ec7a11144ec272b3e989a75ffdbf3f35db61f1
Joseph-dev04 <jpajuelo.dev@gmail.com>
2026-05-11 03:08:38 +0200

    ultimo

--- a_maze_ing.py
+++ a_maze_ing.py
@@ -6,6 +6,7 @@ from src.Direcccion import Direccion
 from src.Celda import Celda
 from src.GameControl import GameControl
 from src.Player import Player
+from src.map import MazeConfig
 from mlx import Mlx
 
 def draw_tile(m, mlx, win, img,x,y):
@@ -13,17 +14,17 @@ def draw_tile(m, mlx, win, img,x,y):
 
 
 
-def load_tiles(m, mlx):
+def load_tiles(m, mlx, cfg:MazeConfig):
     tiles = {}
     for i in range(16):
         key = format(i, "04b")
-        path = f"./img2/{key}.png"
+        path = f"./{cfg.ruta}/{key}.png"
         img,_,_ = m.mlx_png_file_to_image(mlx, path)
         tiles[key] = img
     
-    tiles["entry"],_,_ = m.mlx_png_file_to_image(mlx, "./img/pj.png")
-    tiles["exit"],_,_ = m.mlx_png_file_to_image(mlx, "./img/pj.png")
-    tiles["player"],_,_ = m.mlx_png_file_to_image(mlx, "./img/pj.png")
+    tiles["entry"],_,_ = m.mlx_png_file_to_image(mlx, f"./{cfg.ruta}/entrada.png")
+    tiles["exit"],_,_ = m.mlx_png_file_to_image(mlx, f"./{cfg.ruta}/salida.png")
+    tiles["player"],_,_ = m.mlx_png_file_to_image(mlx, f"./{cfg.ruta}/mario1.png")
     return tiles
 
 def programa(parse_config):
@@ -61,6 +62,7 @@ def close_window(param):
     param.mlx_loop_exit(param.mlx_ptr)
 
 def key_hook(key, control:GameControl):
+    print(key)
     botones = [119,115,97,100]
     if key == 65307:
         control._m.mlx_loop_exit(control._m.mlx_ptr)
@@ -74,6 +76,10 @@ def key_hook(key, control:GameControl):
         control.crear_mapa2()
     elif key == 50:
         control.crear_mapa1()
+    elif key == 32:
+        control.change_color()
+    elif key == 99:
+        control.pintar_ruta()
 
 
 
@@ -95,20 +101,20 @@ if __name__ == '__main__':
         m.mlx_ptr = m.mlx_init()
         win = m.mlx_new_window(
             m.mlx_ptr,
-            len(gen.grid_binario[0]) * 40,
-            (len(gen.grid_binario) * 40)+80,
+            len(gen.grid_binario[0]) * cfg.pixel,
+            (len(gen.grid_binario) * cfg.pixel)+80,
             "42 maze"
         )
-        tiles = load_tiles(m, m.mlx_ptr)
+        tiles = load_tiles(m, m.mlx_ptr, cfg)
         player = Player((cfg.entry_x, cfg.entry_y), tiles["player"])
         control = GameControl(m, win, gen, player, tiles)
         control.crear_mapa2()
 		#ESTO ES SOLO PRUEBA, LUEGO AÑADIRE UN BUEN FUNCIONAMIENTO SOLO ES PARA LA SEPARACION DEL MAPA
         img,_,_ = m.mlx_png_file_to_image(m.mlx_ptr, "./img/play.png")
-        m.mlx_put_image_to_window(m.mlx_ptr, win, img, 30,(len(gen.grid_binario) * 40)+20)
-        m.mlx_put_image_to_window(m.mlx_ptr, win, img, 60+140,(len(gen.grid_binario) * 40)+20)
-        m.mlx_put_image_to_window(m.mlx_ptr, win, img, 90+140+140,(len(gen.grid_binario) * 40)+20)
-        m.mlx_put_image_to_window(m.mlx_ptr, win, img, 120+140+140+140,(len(gen.grid_binario) * 40)+20)
+        m.mlx_put_image_to_window(m.mlx_ptr, win, img, 30,(len(gen.grid_binario) * cfg.pixel)+20)
+        m.mlx_put_image_to_window(m.mlx_ptr, win, img, 60+140,(len(gen.grid_binario) * cfg.pixel)+20)
+        m.mlx_put_image_to_window(m.mlx_ptr, win, img, 90+140+140,(len(gen.grid_binario) * cfg.pixel)+20)
+        m.mlx_put_image_to_window(m.mlx_ptr, win, img, 120+140+140+140,(len(gen.grid_binario) * cfg.pixel)+20)
         m.mlx_key_hook(win, key_hook, control)
         m.mlx_hook(win, 17, 0, close_window, m)
         m.mlx_loop(m.mlx_ptr)
